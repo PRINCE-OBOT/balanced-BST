f9e907dcadc74c6786d80328f134a73c3734213b
PRINCE-OBOT <princessamuel45@gmail.com>
2026-02-07 02:09:18 +0100

    Follow up 'tieing it all together'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import Tree from './tree';
 
-const tree = new Tree([1, 7, 4, 10, 23, 8, 9, 3,  24,  5, 7, 9]);
+const getArrOfRandomNum = (len) => {
+  const arr = [];
 
-tree.insert(2)
-tree.insert(0)
-tree.insert(100)
-tree.insert(120)
-// console.log(tree.isBalance(52));
-tree.rebalance()
+  for (let i = 0; i < len; i++) {
+    arr.push(Math.floor(Math.random() * 100));
+  }
+
+  return arr;
+};
+const tree = new Tree(getArrOfRandomNum(15));
+
+console.log(tree.isBalance()); // true
+
+console.log('preOrderForEach');
+tree.preOrderForEach((data) => console.log(data));
+
+console.log('inOrderForEach');
+tree.inOrderForEach((data) => console.log(data));
+
+console.log('postOrderForEach');
+tree.postOrderForEach((data) => console.log(data));
+
+// Unbalance tree
+tree.insert(100);
+tree.insert(240);
+
+console.log(tree.isBalance()); // false
+
+tree.rebalance();
+
+console.log(tree.isBalance()); // true
+
 tree.prettyPrint(tree.root);
-// console.log(tree.height(67))
